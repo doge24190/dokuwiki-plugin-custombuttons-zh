dab6b2a92f64f32e404f94e1aa5f9399e02f8eb7
doge24190 <69849296+doge24190@users.noreply.github.com>
2024-08-01 17:58:03 +0800

    Add files via upload

---
+++ lang/zh/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['name']              = '定制按钮设置';
+
+$lang['picker']            = '定制按钮';
+
+$lang['txt_error']         = '没有配置文件的写入权限';
+
+$lang['btnslist']          = '按钮列表';
+$lang['btnslist_label']    = '按钮名';
+$lang['btnslist_code']     = '代码示例';
+$lang['btnslist_delete']   = '删除？';
+$lang['btn_delete']        = '删除选中';
+
+$lang['addbtn']            = '增加按钮';
+$lang['addbtn_icon']       = '图标:';
+$lang['addbtn_textonly']   = '仅文字';
+$lang['addbtn_label']      = '按钮名：';
+$lang['addbtn_pretag']     = '代码前缀：';
+$lang['addbtn_posttag']    = '代码后缀：';
+$lang['addbtn_code']       = '默认文本：';
+$lang['btn_add']           = '加入';
+$lang['txt_comment']       = '<b>*</b> 如果你想加入一个按钮不提供前缀和后缀，留空就行。';
---
+++ lang/zh/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author doge24190 <admin@doge24190.top>, 2024
+ */
+$lang['usepicker'] = '在管理页面自定义添加新的编辑工具栏按钮。';
